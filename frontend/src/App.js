80474d40638cb0aa8c668543e0420a0754c2400b
brunoedcf <brunoesteves.dcf@outlook.com>
2024-05-29 14:47:24 -0300

    First commit: Initial frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <h1>Hello, React!</h1>
+      <h1>Hello, React!</h1>
+      <h1>Hello, React!</h1>
+      <h1>Hello, React!</h1>
+      <h1>Hello, React!</h1>
+      <h1>Hello, React!</h1>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+export default App;
