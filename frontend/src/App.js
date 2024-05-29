03817ce6faac76d651fd58c7c09746705c828f34
brunoedcf <brunoesteves.dcf@outlook.com>
2024-05-29 15:12:47 -0300

    First styles and structure for card interface.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import cardData from './data/cardData';
 
 function App() {
+  const [collections, setCollections] = useState([]);
+
+  useEffect(() => {
+    setCollections(cardData);
+  }, []);
+
   return (
-    <div>
-      <h1>Hello, React!</h1>
-      <h1>Hello, React!</h1>
-      <h1>Hello, React!</h1>
-      <h1>Hello, React!</h1>
-      <h1>Hello, React!</h1>
-      <h1>Hello, React!</h1>
-      <h1>Hello, React!</h1>
-      <h1>Hello, React!</h1>
+    <div className="container">
+      <h1>One Piece TCG Cards</h1>
+      {collections.map((collection, index) => (
+        <div className="collection" key={index}>
+          <h2>{collection.Name}</h2>
+          <p>Release Date: {collection['Release Date']}</p>
+          <a href={collection.Link} target="_blank" rel="noopener noreferrer">Collection Link</a>
+          <ul className="cards">
+            {collection.Cards.map((card, cardIndex) => (
+              <li className="card" key={cardIndex}>
+                <img src={`https:${card.Image}`} alt={card.Name} />
+                <div>
+                  <h3>{card.Name}</h3>
+                  <p>Lowest Price: {card['Lowest Price']}</p>
+                  <p>Highest Price: {card['Highest Price']}</p>
+                  <a href={card['Link Marketplace']} target="_blank" rel="noopener noreferrer">Marketplace Link</a>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ frontend/src/data/cardData.js
@@ -0,0 +1,63 @@
+const cardData = [
+    {
+      "Name": "One Piece Promotion Cards",
+      "Acronym": "PC-01",
+      "Release Date": "01/09/2022",
+      "Link": "https://www.ligaonepiece.com.br/?view=cards/search&card=edid=2%20ed=PC-01",
+      "Cards": [
+        {
+          "Name": "Arlong (Premium Card Collection -Live Action Edition-) (P-048-LA)",
+          "Lowest Price": "R$ 39,99",
+          "Highest Price": "R$ 48,99",
+          "Link Marketplace": "https://www.ligaonepiece.com.br/?view=cards/card&card=Arlong%20(Premium%20Card%20Collection%20-Live%20Action%20Edition-)%20(P-048-LA)&ed=PC-01&num=P-048-LA",
+          "Image": "//repositorio.sbrauble.com//arquivos/in/onepiece/2/662bf91fab726-a2ps6-qwckg-a12f16c644039099699332e247f11ec0.jpg"
+        },
+        {
+          "Name": "Arlong (Sealed Battle Kit Vol. 1) (P-048-SB)",
+          "Lowest Price": "R$ 69,99",
+          "Highest Price": "R$ 769,89",
+          "Link Marketplace": "https://www.ligaonepiece.com.br/?view=cards/card&card=Arlong%20(Sealed%20Battle%20Kit%20Vol.%201)%20(P-048-SB)&ed=PC-01&num=P-048-SB",
+          "Image": "//repositorio.sbrauble.com//arquivos/in/onepiece/2/6566597d31fdb-hf3p0-4x93o-a12f16c644039099699332e247f11ec0.jpg"
+        },
+        {
+          "Name": "Backlight (Starter Deck 11: Uta Deck Battle) (ST11-003-UD)",
+          "Lowest Price": "R$ 10,00",
+          "Highest Price": "R$ 19,99",
+          "Link Marketplace": "https://www.ligaonepiece.com.br/?view=cards/card&card=Backlight%20(Starter%20Deck%2011%3A%20Uta%20Deck%20Battle)%20(ST11-003-UD)&ed=PC-01&num=ST11-003-UD",
+          "Image": "//repositorio.sbrauble.com//arquivos/in/onepiece/2/65bd466d632ef-rx5yj-s7dlw-a12f16c644039099699332e247f11ec0.jpg"
+        }
+      ]
+    },
+    {
+      "Name": "Super Pre-Release Starter Deck 1: Straw Hat Crew",
+      "Acronym": "ST-01-PR",
+      "Release Date": "30/09/2022",
+      "Link": "https://www.ligaonepiece.com.br/?view=cards/search&card=edid=16%20ed=ST-01-PR",
+      "Cards": [
+        {
+          "Name": "Brook (ST01-011)",
+          "Lowest Price": "R$ 19,99",
+          "Highest Price": "R$ 19,99",
+          "Link Marketplace": "https://www.ligaonepiece.com.br/?view=cards/card&card=Brook%20(ST01-011)&ed=ST-01-PR&num=ST01-011",
+          "Image": "//repositorio.sbrauble.com//arquivos/in/onepiece/16/641ca366006ec-3vpm2-7194k-7a2ad61e236d8e085bc363a21baff2a6.jpg"
+        },
+        {
+          "Name": "DON!! Card (DON-213-554)",
+          "Lowest Price": "R$ 39,99",
+          "Highest Price": "R$ 39,99",
+          "Link Marketplace": "https://www.ligaonepiece.com.br/?view=cards/card&card=DON!!%20Card%20(DON-213-554)&ed=ST-01-PR&num=DON-213-554",
+          "Image": "//repositorio.sbrauble.com//arquivos/in/onepiece/16/641ca366b4ccb-fagqm-125wd-5cd7acf90259173b996b6d5f162702ea.jpg"
+        },
+        {
+          "Name": "Diable Jambe (ST01-016)",
+          "Lowest Price": "R$ 1,65",
+          "Highest Price": "R$ 1,65",
+          "Link Marketplace": "https://www.ligaonepiece.com.br/?view=cards/card&card=Diable%20Jambe%20(ST01-016)&ed=ST-01-PR&num=ST01-016",
+          "Image": "//repositorio.sbrauble.com//arquivos/in/onepiece/16/641ca36a5a534-6c1iv-yjozs-cb1da1774e018a17e6a880f83b4afd1b.jpg"
+        }
+      ]
+    }
+  ];
+  
+  export default cardData;
+  
